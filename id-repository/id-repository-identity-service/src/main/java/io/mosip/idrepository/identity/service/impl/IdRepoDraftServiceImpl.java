3dce9101040dc8c614fee3c1510420d14026c6fb
Shivam Rajesh Gupta <56411166+shivamg9@users.noreply.github.com>
2026-03-20 20:59:46 +0530

    Add Iterator import to IdRepoDraftServiceImpl
    Signed-off-by: Shivam Rajesh Gupta <56411166+shivamg9@users.noreply.github.com>

--- id-repository/id-repository-identity-service/src/main/java/io/mosip/idrepository/identity/service/impl/IdRepoDraftServiceImpl.java
+++ id-repository/id-repository-identity-service/src/main/java/io/mosip/idrepository/identity/service/impl/IdRepoDraftServiceImpl.java
@@ -27,6 +27,7 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.Iterator;
 
 import org.hibernate.exception.JDBCConnectionException;
 import org.json.JSONException;
